466406839bcd23a8c3f8b45245ccbf9b472cfb22
Pawan Kumar <pk.soft29@gmail.com>
2020-09-10 11:10:10 +0530

    Modify : flipCoinSimulator.sh modified with won by dufference or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-read -p "Enter the number of time to toss" num
 HEAD_COUNT=0;
 TAIL_COUNT=0;
 Winner=1;
-for (( i=1; i <= $num; i++ ))
+Loose=0;
+while :
 do
 	outCome=$((RANDOM%2))
 
-	if [ $outCome -eq $Winner ]
+	if [[ $outCome -eq $Winner  && $HEAD_COUNT -lt 21 ]]
 	then
 		HEAD_COUNT=$((HEAD_COUNT + 1))
-	else
+	elif [[ $outCome -le $Loose  && $TAIL_COUNT -lt 21 ]]
+	then
 		TAIL_COUNT=$((TAIL_COUNT +1 ))
+	else
+		break;
 	fi
 done
+if [ $HEAD_COUNT -gt $TAIL_COUNT ]
+then
+	diff=$(($HEAD_COUNT - $TAIL_COUNT))
+	echo "Head Won by : $diff Point"
+elif [ $TAIL_COUNT -gt $HEAD_COUNT ]
+then
+	diff=$(($TAIL_COUNT - $HEAD_COUNT))
+	echo "Tail Won by : $diff Pint"
+else
+	echo "Its Tie"
 
-echo "Head Won : $HEAD_COUNT Times"
-
-echo "Tail Won : $TAIL_COUNT Times"
+fi
