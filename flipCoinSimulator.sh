fd51c8eca9c8eb4b24d62f15e42df89eb37b9b6f
Pawan Kumar <pk.soft29@gmail.com>
2020-09-10 12:30:54 +0530

    Added : flipCoinSimulator.sh Tie  condition added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,18 +4,30 @@ HEAD_COUNT=0;
 TAIL_COUNT=0;
 Winner=1;
 Loose=0;
+diff=0;
 while :
 do
 	outCome=$((RANDOM%2))
 
-	if [[ $outCome -eq $Winner  && $HEAD_COUNT -lt 21 ]]
+	if [ $outCome -eq $Winner ]
 	then
+		
 		HEAD_COUNT=$((HEAD_COUNT + 1))
-	elif [[ $outCome -le $Loose  && $TAIL_COUNT -lt 21 ]]
+	elif [ $outCome -le $Loose ]
 	then
 		TAIL_COUNT=$((TAIL_COUNT +1 ))
+	fi
+	if [[ $HEAD_COUNT -ge 21 || $TAIL_COUNT -ge 21 ]]
+	then
+		if [[ `expr $TAIL_COUNT - $HEAD_COUNT` -gt 2 || `expr $HEAD_COUNT - $TAIL_COUNT` -gt 2 ]]
+		then	
+			break;
+		else
+			echo "Tie"
+			continue;
+		fi
 	else
-		break;
+		continue
 	fi
 done
 if [ $HEAD_COUNT -gt $TAIL_COUNT ]
@@ -25,7 +37,7 @@ then
 elif [ $TAIL_COUNT -gt $HEAD_COUNT ]
 then
 	diff=$(($TAIL_COUNT - $HEAD_COUNT))
-	echo "Tail Won by : $diff Pint"
+	echo "Tail Won by : $diff Point"
 else
 	echo "Its Tie"
 
